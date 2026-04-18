e55ef202025d4b6169743e045240101f7c392f03
misaka <zelda.me@outlook.com>
2026-04-18 18:06:27 +0800

    1

--- src/config.ts
+++ src/config.ts
@@ -1,177 +1,177 @@
 import type {
-	GitHubEditConfig,
-	ImageFallbackConfig,
-	LicenseConfig,
-	NavBarConfig,
-	ProfileConfig,
-	SiteConfig,
-	UmamiConfig,
+  GitHubEditConfig,
+  ImageFallbackConfig,
+  LicenseConfig,
+  NavBarConfig,
+  ProfileConfig,
+  SiteConfig,
+  UmamiConfig,
 } from "./types/config";
 import { LinkPreset } from "./types/config";
 
-const customDomain = "2x.nz";
+const customDomain = "blog.cn3.top";
 
 export const siteConfig: SiteConfig = {
-	customDomain,
-	title: "《二叉树树》官方网站",
-	subtitle: "AcoFork",
-	description:
-		"《二叉树树》是一个专注于IT/互联网技术分享与实践的个人技术博客，在这里你可以找到众多前沿技术的分享与实践经验。",
+  customDomain,
+  title: "《二叉树树》官方网站",
+  subtitle: "AcoFork",
+  description:
+    "《二叉树树》是一个专注于IT/互联网技术分享与实践的个人技术博客，在这里你可以找到众多前沿技术的分享与实践经验。",
 
-	keywords: [
-		"二叉树树",
-		"二叉树树官网",
-		"树",
-		"二叉树",
-		"二叉",
-		"博客",
-		"AcoFork Blog",
-		"AcoFork",
-		"Blog",
-		"acofork blog",
-		"acofork",
-		"blog",
-	],
-	lang: "zh_CN", // 'en', 'zh_CN', 'zh_TW', 'ja', 'ko', 'es', 'th'
-	themeColor: {
-		hue: 255, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
-		fixed: true, // Hide the theme color picker for visitors
-	},
-	banner: {
-		enable: false,
-		src: "/xinghui.avif", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+  keywords: [
+    "二叉树树",
+    "二叉树树官网",
+    "树",
+    "二叉树",
+    "二叉",
+    "博客",
+    "AcoFork Blog",
+    "AcoFork",
+    "Blog",
+    "acofork blog",
+    "acofork",
+    "blog",
+  ],
+  lang: "zh_CN", // 'en', 'zh_CN', 'zh_TW', 'ja', 'ko', 'es', 'th'
+  themeColor: {
+    hue: 255, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
+    fixed: true, // Hide the theme color picker for visitors
+  },
+  banner: {
+    enable: false,
+    src: "/xinghui.avif", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
 
-		position: "center", // Equivalent to object-position, only supports 'top', 'center', 'bottom'. 'center' by default
-		credit: {
-			enable: true, // Display the credit text of the banner image
-			text: "Pixiv @chokei", // Credit text to be displayed
+    position: "center", // Equivalent to object-position, only supports 'top', 'center', 'bottom'. 'center' by default
+    credit: {
+      enable: true, // Display the credit text of the banner image
+      text: "Pixiv @chokei", // Credit text to be displayed
 
-			url: "https://www.pixiv.net/artworks/122782209", // (Optional) URL link to the original artwork or artist's page
-		},
-	},
-	background: {
-		enable: false, // Enable background image
-		src: "", // Background image URL (supports HTTPS)
-		position: "center", // Background position: 'top', 'center', 'bottom'
-		size: "cover", // Background size: 'cover', 'contain', 'auto'
-		repeat: "no-repeat", // Background repeat: 'no-repeat', 'repeat', 'repeat-x', 'repeat-y'
-		attachment: "fixed", // Background attachment: 'fixed', 'scroll', 'local'
-		opacity: 1, // Background opacity (0-1)
-	},
-	toc: {
-		enable: true, // Display the table of contents on the right side of the post
-		depth: 2, // Maximum heading depth to show in the table, from 1 to 3
-	},
-	favicon: [
-		// Leave this array empty to use the default favicon
-		{
-			src: "https://q2.qlogo.cn/headimg_dl?dst_uin=2726730791&spec=0", // Path of the favicon, relative to the /public directory
-			//   sizes: '32x32',              // (Optional) Size of the favicon, set only if you have favicons of different sizes
-		},
-	],
-	officialSites: [
-		{ url: "https://acofork.com", alias: "CN" },
-		{ url: `https://${customDomain}`, alias: "Global" },
-	],
-	server: [
-		{ url: "", text: "Blog" },
-		{ url: `https://u.${customDomain}`, text: "Umami" },
-		{ url: `https://p.${customDomain}`, text: "RandomPic" },
-	],
+      url: "https://www.pixiv.net/artworks/122782209", // (Optional) URL link to the original artwork or artist's page
+    },
+  },
+  background: {
+    enable: false, // Enable background image
+    src: "", // Background image URL (supports HTTPS)
+    position: "center", // Background position: 'top', 'center', 'bottom'
+    size: "cover", // Background size: 'cover', 'contain', 'auto'
+    repeat: "no-repeat", // Background repeat: 'no-repeat', 'repeat', 'repeat-x', 'repeat-y'
+    attachment: "fixed", // Background attachment: 'fixed', 'scroll', 'local'
+    opacity: 1, // Background opacity (0-1)
+  },
+  toc: {
+    enable: true, // Display the table of contents on the right side of the post
+    depth: 2, // Maximum heading depth to show in the table, from 1 to 3
+  },
+  favicon: [
+    // Leave this array empty to use the default favicon
+    {
+      src: "https://q2.qlogo.cn/headimg_dl?dst_uin=2726730791&spec=0", // Path of the favicon, relative to the /public directory
+      //   sizes: '32x32',              // (Optional) Size of the favicon, set only if you have favicons of different sizes
+    },
+  ],
+  officialSites: [
+    { url: "https://acofork.com", alias: "CN" },
+    { url: `https://${customDomain}`, alias: "Global" },
+  ],
+  server: [
+    { url: "", text: "Blog" },
+    { url: `https://u.${customDomain}`, text: "Umami" },
+    { url: `https://p.${customDomain}`, text: "RandomPic" },
+  ],
 };
 
 export const navBarConfig: NavBarConfig = {
-	links: [
-		LinkPreset.Home,
-		LinkPreset.Archive,
-		{
-			name: "友链",
-			url: "/friends/",
-			external: false,
-			icon: "material-symbols:group-outline-rounded",
-		},
-		{
-			name: "赞助",
-			url: "/sponsors/",
-			external: false,
-			icon: "material-symbols:volunteer-activism-outline-rounded",
-		},
-		{
-			name: "工具",
-			url: "/tools/",
-			external: false,
-			icon: "material-symbols:build-outline-rounded",
-		},
-		{
-			name: "统计",
-			url: `https://u.${customDomain}/share/CdkXbGgZr6ECKOyK`,
-			external: true,
-			icon: "material-symbols:table-chart",
-		},
-		{
-			name: "论坛",
-			url: "/forum/",
-			external: false,
-			icon: "material-symbols:forum-outline-rounded",
-		},
-	],
+  links: [
+    LinkPreset.Home,
+    LinkPreset.Archive,
+    {
+      name: "友链",
+      url: "/friends/",
+      external: false,
+      icon: "material-symbols:group-outline-rounded",
+    },
+    {
+      name: "赞助",
+      url: "/sponsors/",
+      external: false,
+      icon: "material-symbols:volunteer-activism-outline-rounded",
+    },
+    {
+      name: "工具",
+      url: "/tools/",
+      external: false,
+      icon: "material-symbols:build-outline-rounded",
+    },
+    {
+      name: "统计",
+      url: `https://u.${customDomain}/share/CdkXbGgZr6ECKOyK`,
+      external: true,
+      icon: "material-symbols:table-chart",
+    },
+    {
+      name: "论坛",
+      url: "/forum/",
+      external: false,
+      icon: "material-symbols:forum-outline-rounded",
+    },
+  ],
 };
 
 export const profileConfig: ProfileConfig = {
-	avatar: "https://q2.qlogo.cn/headimg_dl?dst_uin=2726730791&spec=0", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
-	name: "二叉树树",
-	bio: "Protect What You Love.",
-	links: [
-		{
-			name: "QQ",
-			icon: "qq", // Local icon
-			url: "https://qm.qq.com/q/FWqOHlwL2m",
-		},
-		{
-			name: "Telegram",
-			icon: "telegram", // Local icon
-			url: "https://t.me/+_07DERp7k1ljYTc1",
-		},
-		{
-			name: "Bilibli",
-			icon: "bilibili", // Local icon
-			url: "https://space.bilibili.com/325903362",
-		},
-		{
-			name: "GitHub",
-			icon: "github", // Local icon
-			url: "https://github.com/afoim",
-		},
-		{
-			name: "Folo",
-			icon: "folo", // Local icon
-			url: "https://app.folo.is/share/feeds/245004133358075904",
-		},
-	],
+  avatar: "https://q2.qlogo.cn/headimg_dl?dst_uin=2726730791&spec=0", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+  name: "二叉树树",
+  bio: "Protect What You Love.",
+  links: [
+    {
+      name: "QQ",
+      icon: "qq", // Local icon
+      url: "https://qm.qq.com/q/FWqOHlwL2m",
+    },
+    {
+      name: "Telegram",
+      icon: "telegram", // Local icon
+      url: "https://t.me/+_07DERp7k1ljYTc1",
+    },
+    {
+      name: "Bilibli",
+      icon: "bilibili", // Local icon
+      url: "https://space.bilibili.com/325903362",
+    },
+    {
+      name: "GitHub",
+      icon: "github", // Local icon
+      url: "https://github.com/afoim",
+    },
+    {
+      name: "Folo",
+      icon: "folo", // Local icon
+      url: "https://app.folo.is/share/feeds/245004133358075904",
+    },
+  ],
 };
 
 export const licenseConfig: LicenseConfig = {
-	enable: true,
-	name: "CC BY-NC-SA 4.0",
-	url: "https://creativecommons.org/licenses/by-nc-sa/4.0/",
+  enable: true,
+  name: "CC BY-NC-SA 4.0",
+  url: "https://creativecommons.org/licenses/by-nc-sa/4.0/",
 };
 
 export const imageFallbackConfig: ImageFallbackConfig = {
-	enable: false,
-	originalDomain: "https://eopfapi.acofork.com/pic?img=ua",
-	fallbackDomain: "https://eopfapi.acofork.com/pic?img=ua",
+  enable: false,
+  originalDomain: "https://eopfapi.acofork.com/pic?img=ua",
+  fallbackDomain: "https://eopfapi.acofork.com/pic?img=ua",
 };
 
 export const umamiConfig: UmamiConfig = {
-	enable: true,
-	baseUrl: `https://u.${customDomain}`,
-	shareId: "CdkXbGgZr6ECKOyK",
-	timezone: "Asia/Shanghai",
+  enable: true,
+  baseUrl: `https://u.${customDomain}`,
+  shareId: "CdkXbGgZr6ECKOyK",
+  timezone: "Asia/Shanghai",
 };
 
 export const gitHubEditConfig: GitHubEditConfig = {
-	enable: true,
-	baseUrl: "https://github.com/afoim/fuwari/blob/main/src/content/posts",
+  enable: true,
+  baseUrl: "https://github.com/afoim/fuwari/blob/main/src/content/posts",
 };
 
 // todoConfig removed from here
