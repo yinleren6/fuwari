a17caa248037630bcd1fbd8844599768ada3c788
二叉树树 <acofork@qq.com>
2026-04-19 20:46:30 +0800

    fix: 修复 CDN 化脚本中重复的 public 路径

--- scripts/cdnify-images.js
+++ scripts/cdnify-images.js
@@ -9,7 +9,7 @@ import {
 } from "./utils/content-files.js";
 
 const OLD_PATH_PATTERN = /\/assets\/images\/+/g;
-const NEW_PATH = "https://cnb.cool/2x.nz/fuwari/-/git/raw/main/public/public/assets/images/";
+const NEW_PATH = "https://cnb.cool/2x.nz/fuwari/-/git/raw/main/public/assets/images/";
 
 async function getAllMarkdownFiles() {
 	try {
