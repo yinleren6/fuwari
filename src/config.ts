320a7bb00d30d27eec7b0a31529b1bc1a7c0e849
yinleren6 <52646375+yinleren6@users.noreply.github.com>
2026-04-22 16:15:58 +0800

    Update siteConfig for new blog title and keywords

--- src/config.ts
+++ src/config.ts
@@ -13,23 +13,17 @@ const customDomain = "blog.cn3.top";
 
 export const siteConfig: SiteConfig = {
   customDomain,
-  title: "《二叉树树》官方网站",
-  subtitle: "AcoFork",
+  title: "《嘻嘻》官方网站",
+  subtitle: "CN3",
   description:
-    "《二叉树树》是一个专注于IT/互联网技术分享与实践的个人技术博客，在这里你可以找到众多前沿技术的分享与实践经验。",
+    "《嘻嘻》是一个专注于IT/互联网技术分享与实践的个人技术博客，在这里你可以找到众多前沿技术的分享与实践经验。",
 
   keywords: [
-    "二叉树树",
-    "二叉树树官网",
-    "树",
-    "二叉树",
-    "二叉",
+    "嘻嘻",
+    "关键词",
+    "懒得改",
     "博客",
-    "AcoFork Blog",
-    "AcoFork",
     "Blog",
-    "acofork blog",
-    "acofork",
     "blog",
   ],
   lang: "zh_CN", // 'en', 'zh_CN', 'zh_TW', 'ja', 'ko', 'es', 'th'
